7e3bfd23e65eec173eb6a0e9d6c7032a22058e90
Rafael98723 <rafadesousa90@gmail.com>
2025-08-14 14:39:00 +0200

    Shoe store

--- apprenticeship-trial-2026/schnupperlehre/app/Dynamische Schuhe/script.js
+++ apprenticeship-trial-2026/schnupperlehre/app/Dynamische Schuhe/script.js
@@ -1,18 +1,18 @@
 const Schuhe = [
     {
-        bild:"./images/Kobes.avif",
+        bild:"./Images/Kobes.avif",
         beschreibung:"Gute leistungs Schuhe!",
         titel:"Nike Kobe 6 All Stars",
         preis:"200 Fr.",
         id:1
     },{
-        bild:"./images/grinches.jpg",
+        bild:"./Images/grinches.jpeg",
         beschreibung:"Sehr gute Schuhe. Kaufen sie die Schuhe Heute!",
         titel:"Nike Kobe 6 Grinches",
         preis:"540 Fr.",
         id:2
     },{
-        bild:"./images/Totalorange.jpg",
+        bild:"./Images/Kobe6rev.webp",
         beschreibung:"Sehr bunte und gute Schuhe. Kaufen sie noch Heute!",
         titel:"Nike Kobe 6 Total Orange",
         preis:"210 Fr.",
@@ -31,17 +31,20 @@ function zeigeShuhkachel(){
     slot="image"
     src="${Schuhe.bild}"
     alt="Basketball shoes"
+    widht= "500px"
+    height= "333.13px"
   />
 
   <strong>${Schuhe.titel}</strong>
   <br> ${Schuhe.beschreibung}<br />
- 
+  <br>${Schuhe.preis}</br>
 
   <div slot="footer">
     <sl-button variant="primary" pill>More Info</sl-button>
     <sl-rating></sl-rating>
   </div>
 </sl-card>`
+
     container.appendChild(kachel)
 })
 }
